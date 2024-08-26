f0865505fd8da0177f397527c0fb2cfdb20b814a
MitrovicJr <makar1.mitrovic@gmail.com>
2024-08-26 17:18:37 +0200

    Updated scraping.py file with annotations

--- scraping.py
+++ scraping.py
@@ -4,43 +4,44 @@ import pandas as pd
 from io import StringIO
 import os
 
-# List of URLs to scrape
+#Saving the URL's of websites to scrape
 urls = [
     f'https://www.swimrankings.net/index.php?page=meetDetail&meetId=626385&gender=1&styleId={i}' 
     for i in range(1, 20)
 ]
 
-# Column names for DataFrame
+# Column names for each Dataframe
 column_names = ['Place', 'Name', 'Year of birth', 'Country code', 'Country', 'Time', 'Event']
 
 def scrape_meet_results(url):
-    """Scrape results from a given URL and return a list of DataFrames."""
+    #Finds all relevant 'table' elements from the url and saves them into the 'tables' variable
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     tables = soup.find_all('table', class_='meetResult')
 
+    #Saves the tables into a Pandas Dataframe and adds them to the list of dataframes
     dataframes = []
     for table in tables:
-        df = pd.read_html(StringIO(str(table)))[0].iloc[:, :-2]
-        event = df.columns[0]
-        df.columns = column_names[:-1]  # Exclude the 'Event' column for now
-        df['Event'] = event
+        df = pd.read_html(StringIO(str(table)))[0].iloc[:, :-2] #Excludes the last 2 columns, which are irrelevant for the analysis
+        event = df.columns[0] #Extracts the event name
+        df.columns = column_names[:-1]
+        df['Event'] = event #Saves the event name into a new column
         dataframes.append(df)
 
     return dataframes
 
-# Collect all results
+# Runs the relevant function on all saved URL's
 all_dataframes = [df for url in urls for df in scrape_meet_results(url)]
 
-# Combine DataFrames
+# Concatenates the dataframes into one
 result_df = pd.concat(all_dataframes, axis=0, ignore_index=True)
 
 # Ensure the output directory exists
 output_dir = 'results'
 os.makedirs(output_dir, exist_ok=True)
 
-# Save the result DataFrame to CSV
+# Saves the result DataFrame to a csv file
 result_df.to_csv(os.path.join(output_dir, 'result_data.csv'), index=False)
 
-# Display the first 5 rows of the DataFrame
+# prints the first 5 elements of the concatenated dataframe, to check for abnormalities
 print(result_df.head(5))
